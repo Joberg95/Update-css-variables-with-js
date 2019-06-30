8c359577accecea481fa7a1fb50b9c07d6c45a36
nevroje <joakim.berglind@gmail.com>
2019-06-30 16:14:03 +0200

    created mousemove eventlitsener for console. add suffix dataset sizing

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,7 +1,12 @@
 const input = document.querySelectorAll(".controls input");
 
 function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
   console.log(this.value);
+  console.log(suffix);
+  console.log(this.dataset);
+
 }
 
 input.forEach(input => input.addEventListener("change", handleUpdate));
+input.forEach(input => input.addEventListener("mousemove", handleUpdate));
